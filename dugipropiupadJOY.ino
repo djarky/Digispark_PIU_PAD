e4909e816833b160b70daab4f05511e4547471ef
DJ ARKY <aivr_95@hotmail.com>
2023-04-29 03:06:59 -0400

    Add files via upload

--- dugipropiupadJOY.ino
+++ dugipropiupadJOY.ino
@@ -4,21 +4,24 @@
 
 //#include "DigiKeyboard.h"
 
-#define UL 0
-#define UR 1
-#define CE 2
-#define DL 3
-#define DR 4
+#define UL 12
+#define UR 11
+#define CE 10
+#define DL 9
+#define DR 8
 
-#define CC 5
+#define COIN 7
+
+unsigned char Joy_status=0x00;
 
 
 void setup() {      
-  pinMode(UL, INPUT_PULLUP); //button0 
-  pinMode(UR, INPUT_PULLUP); //button1
-  pinMode(CE, INPUT_PULLUP); //button2
-  pinMode(DL, INPUT_PULLUP); //button3
-  pinMode(DR, INPUT_PULLUP); //button4
+  pinMode(UL,   INPUT_PULLUP); //button0 
+  pinMode(UR,   INPUT_PULLUP); //button1
+  pinMode(CE,   INPUT_PULLUP); //button2
+  pinMode(DL,   INPUT_PULLUP); //button3
+  pinMode(DR,   INPUT_PULLUP); //button4
+  pinMode(COIN, INPUT_PULLUP); //button5
 }
 
 
@@ -31,27 +34,25 @@ Joy_status=0x00;
 
 
 if(digitalRead(UL)==LOW){
-    Joy_status+=0x01
-   //DigiJoystick.setButtons((char)0x00,(char)0x01);    
+    Joy_status+=0x01; 
 }
 if(digitalRead(UR)==LOW){
-    Joy_status+=0x02
-   //DigiJoystick.setButtons((char)0x00,(char)0x02);    
+    Joy_status+=0x02;    
 }
 if(digitalRead(CE)==LOW){
-    Joy_status+=0x04
-   //DigiJoystick.setButtons((char)0x00,(char)0x04);    
+    Joy_status+=0x04;   
 }
 if(digitalRead(DL)==LOW){
-    Joy_status+=0x08
-   //DigiJoystick.setButtons((char)0x00,(char)0x08);    
+    Joy_status+=0x08;
 }
 if(digitalRead(DR)==LOW){
-    Joy_status+=0x10
-   //DigiJoystick.setButtons((char)0x00,(char)0x10);    
+    Joy_status+=0x10;
 }
-
-
-DigiJoystick.setButtons((char)i,(char)Joy_status);
-
+if(digitalRead(COIN)==LOW){
+    Joy_status+=0x20;
+}
+//J1 or J2
+//DigiJoystick.setButtons((char)Joy_status,(char)0);
+DigiJoystick.setButtons((char)0,(char)Joy_status);
+DigiJoystick.delay(1);
 } 
